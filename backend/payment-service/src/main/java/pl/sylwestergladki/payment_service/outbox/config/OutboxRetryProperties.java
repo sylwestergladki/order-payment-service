2cf42f334906f684e045e2426f5bbf79160feb1a
SG <sylwester.gladki@gmail.com>
2026-06-25 20:02:39 +0200

    fix(payment-outbox): fix OutboxRetryProperties

--- backend/payment-service/src/main/java/pl/sylwestergladki/payment_service/outbox/config/OutboxRetryProperties.java
+++ backend/payment-service/src/main/java/pl/sylwestergladki/payment_service/outbox/config/OutboxRetryProperties.java
@@ -1,13 +1,11 @@
 package pl.sylwestergladki.payment_service.outbox.config;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @ConfigurationProperties(prefix = "outbox.retry")
 public class OutboxRetryProperties {
 
