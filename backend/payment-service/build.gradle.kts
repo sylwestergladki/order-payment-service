f9efd48ed32168c96b8d2dbfa8205bd591b0293f
SG <sylwester.gladki@gmail.com>
2026-08-12 19:18:36 +0200

    fix(payment): remove unused dependencies

--- backend/payment-service/build.gradle.kts
+++ backend/payment-service/build.gradle.kts
@@ -21,8 +21,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-starter-data-redis")
-	implementation("org.springframework.boot:spring-boot-starter-cache")
 	implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
 	implementation("org.springframework.kafka:spring-kafka")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
